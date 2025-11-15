b4e3565a77ffa9c540a56c1019b82c4ccfa480a4
Teodor Datcu <teodordatcu.dtc@gmail.com>
2025-11-15 13:35:18 +0200

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,15 +13,7 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
-          media="print"
-          onLoad="this.media='all'"
         />
-        <noscript>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
-            rel="stylesheet"
-          />
-        </noscript>
       </Head>
       <body>
         <Main />
